6ae89a5d845d9062e28c2c181838016ed7825512
Avishek Sen Gupta <avishek.sen.gupta@gmail.com>
2021-04-07 14:21:24 +0530

    Adding test code

--- code/test/test2.py
+++ code/test/test2.py
@@ -1,4 +1,10 @@
 import unittest
+from functools import reduce
+
+def xxx(acc, exists):
+    print(f'acc={acc}')
+    print(f'exists={exists}')
+    return acc or exists
 
 class SomeTest(unittest.TestCase):
     def test_isupper(self):
@@ -47,4 +53,61 @@ class SomeTest(unittest.TestCase):
         print({'a':1, 'b':2})
 
         set_1 = set([1,2,3,4])
-        set_1.symmetric_difference()
+
+        input_str = 'ABCD'
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        result = reduce(lambda acc, exists: acc or exists, list(map(lambda vowel: input_str[0].lower() == vowel, vowels)), False)
+        print("YES" if result else "NO")
+
+
+        for k, v in dict(a=10, b=20).items():
+            print(f'{k}={v}')
+        for i in dict(a=10, b=20):
+            print(i)
+
+        d = {0, 1, 2}
+        print('result of add1')
+        print(d.add(45))
+        print('result of add2')
+        print(d.add(45))
+        print('---------------------------')
+        for x in d:
+            print(x)
+            # print(d.add(x))
+
+        print(list(range(1,6)))
+
+        squared = lambda x,y: x**y
+        print(list(zip([1,2,3], [4,5,6])))
+        first_names = ['A', 'B', 'C']
+        last_names = ['X', 'Y', 'Z']
+        names = list(map(lambda t: f'{t[0]} {t[1]}', list(zip(first_names, last_names))))
+        print(names)
+
+        input_list = ['AAAA', 'BBBB', 'CCCC']
+        print(reduce(lambda acc, x: acc + ' ' + x, input_list, '').strip())
+        print(list(range(1, 5)))
+        n = 5
+        def u(acc, x):
+            print(f'acc={acc}')
+            print(f'x={x}')
+            return acc*x
+        # print(reduce(lambda acc, x: acc*x, list(range(n+1)), 2))
+        print(reduce(u, list(range(1, n+1)), 1))
+
+        C = [2, 5, 9, 12, 13, 15, 16, 17, 18, 19]
+        F = [2, 4, 5, 6, 7, 9, 13, 16]
+        H = [1, 2, 5, 9, 10, 11, 12, 13, 15]
+        cSet = set(C)
+        fSet = set(F)
+        hSet = set(H)
+        all = set(range(1, 21))
+        all3sports = cSet.intersection(fSet).intersection(hSet)
+        c_and_f_but_not_h = cSet.intersection(fSet).intersection(all.difference(hSet))
+        exactly_2 = filter(lambda x: x[1] == 2, list(map(lambda s: (s, (1 if s in cSet else 0) + (1 if s in fSet else 0) + (1 if s in hSet else 0)), all)))
+        no_sports = all.difference(cSet.union(fSet).union(hSet))
+
+        print(sorted(list(all3sports)))
+        print(sorted(list(c_and_f_but_not_h)))
+        print(sorted(list(map(lambda x: x[0], exactly_2))))
+        print(sorted(list(no_sports)))
