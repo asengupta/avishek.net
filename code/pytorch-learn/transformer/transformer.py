62f7e1eeb4f9f1d7ea603341c1fbd66c2b581179
Avishek Sen Gupta <avishek.sen.gupta@gmail.com>
2022-12-01 23:56:11 +0530

    Building SelfAttention Layer

--- code/pytorch-learn/transformer/transformer.py
+++ code/pytorch-learn/transformer/transformer.py
@@ -3,17 +3,27 @@ import torch.nn.functional as F
 import torch.nn as nn
 
 
+softmax = torch.nn.Softmax(dim=1)
+
 def coder_stack(CoderCtor, num_encoders):
     if (num_encoders == 0):
         return None
     return CoderCtor(coder_stack(CoderCtor, num_encoders - 1))
 
 
+class SelfAttentionLayer(nn.Module):
+    def __init__(self, w_q, w_k, w_v):
+        self.w_q = w_q
+        self.w_k = w_k
+        self.w_v = w_v
+    def forward(self, words):
+        return attention_scores(qkvs(words))
+
 class EncoderCtor(nn.Module):
     def __init__(self, next):
         super(EncoderCtor, self).__init__()
         self.next = next
-        self_attention_layer = nn.Linear(1, 1, bias=True)
+        self_attention_layer = SelfAttentionLayer(W_Q, W_K, W_V)
         feedforward_layer = nn.Linear(1, 1, bias=True)
         self.stack = nn.Sequential(self_attention_layer, feedforward_layer)
 
@@ -33,24 +43,12 @@ W_K = torch.randn([word_width, 64]) / 100
 W_V = torch.randn([word_width, 64]) / 100
 
 
-def qkv(word):
-    return torch.matmul(word, W_Q), torch.matmul(word, W_K), torch.matmul(word, W_V)
-
 def qkvs(words):
     return torch.matmul(words, W_Q), torch.matmul(words, W_K), torch.matmul(words, W_V)
 
 
-def attention_score(qkv):
-    return torch.dot(qkv[0], qkv[1]) / 8
-
-softmax = torch.nn.Softmax(dim=1)
 def attention_scores(qkvs):
     return torch.matmul(softmax(torch.matmul(qkvs[0], torch.transpose(qkvs[1], 0, 1)) / 8.), qkvs[2])
-
-
-def softmax_scores(scores):
-    softmax = torch.nn.Softmax(dim=0)
-    return softmax(scores)
 
 
 start_encoder = coder_stack(EncoderCtor, 6)
@@ -61,4 +59,5 @@ word = torch.ones(word_width)
 words = torch.randn([num_words, word_width])
 qkv_words = qkvs(words)
 print(attention_scores(qkv_words))
+print(SelfAttentionLayer(W_Q, W_K, W_V).forward(words))
 
