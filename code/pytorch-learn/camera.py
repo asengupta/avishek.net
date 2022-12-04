9d38bbd20f8f8bdd7a2779cf79daa2e5e184797c
Avishek Sen Gupta <avishek.sen.gupta@gmail.com>
2022-12-04 17:45:30 +0530

    Playing the camera model

--- code/pytorch-learn/camera.py
+++ code/pytorch-learn/camera.py
@@ -2,7 +2,8 @@ import math
 
 import torch
 
-point0 = torch.transpose(torch.tensor([[10., 10., 0., 1.]]), 0, 1)
+point0 = torch.transpose(torch.tensor([[10., 10., 15., 1.]]), 0, 1)
+point0_dash = torch.transpose(torch.tensor([[30., 30., 15., 1.]]), 0, 1)
 point1 = torch.transpose(torch.tensor([[1, 1, 1, 1]]), 0, 1)
 point2 = torch.transpose(torch.tensor([[1., 1., 0., 1.]]), 0, 1)
 
@@ -39,11 +40,13 @@ def translate(x, y, z):
 print(torch.matmul(translate(20, 30, 40), point1))
 print(torch.matmul(rotation_z_axis(math.pi/4), point2))
 
-focal_length = 10.
+focal_length = 1.
 
-camera_coordinate_system = torch.transpose(torch.tensor([[1.,1.,0.,0.], [-1.,1.,0,0.], [.0,0.,1.,0.], [0.,0.,0.,1.]]),0,1)
+
+# camera_coordinate_system = torch.transpose(torch.tensor([[1.,1.,0.,0.], [-1.,1.,0,0.], [.0,0.,1.,0.], [0.,0.,0.,1.]]),0,1)
+camera_coordinate_system = torch.transpose(torch.tensor([[1.,-1.,0.,0.], [0.,0.,1.,0.], [1.,1.,0.,0.], [0.,0.,0.,1.]]),0,1)
 # camera_coordinate_system = torch.transpose(torch.tensor([[math.cos(math.pi/4),math.sin(math.pi/4),0.,0.], [-math.cos(math.pi/4),math.sin(math.pi/4),0,0.], [.0,0.,1.,0.], [0.,0.,0.,1.]]),0,1)
-camera_center = torch.tensor([5.,5.,0.,1.])
+camera_center = torch.tensor([0.,0.,0.,1.])
 camera_center[:3] = camera_center[:3] * -1
 print(camera_center)
 camera_origin_translation = torch.eye(4, 4)
@@ -54,4 +57,11 @@ print(change_of_basis_to_camera)
 point_in_new_basis = torch.matmul(change_of_basis_to_camera, point0)
 print(point_in_new_basis)
 
-intrinsic_camera_parameters = torch.tensor([[]])
+intrinsic_camera_parameters = torch.tensor([[focal_length, 0., 0., 0.],
+                                            [0., focal_length, 0., 0.],
+                                            [0., 0., 1., 0.],
+                                            [0., 0., 0., 1.]])
+
+rendered_point = torch.matmul(intrinsic_camera_parameters, point_in_new_basis)
+point_z = rendered_point[2, 0]
+print(rendered_point / point_z)
