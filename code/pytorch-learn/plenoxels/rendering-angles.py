b2ffc22ffcf049ffc616e0f941192eefc3bc50c3
Avishek Sen Gupta <avishek.sen.gupta@gmail.com>
2022-12-31 22:42:31 +0530

    Fixing bug in generating training angles, torch.unique() rearranges tensors

--- code/pytorch-learn/plenoxels/rendering-angles.py
+++ code/pytorch-learn/plenoxels/rendering-angles.py
@@ -14,8 +14,8 @@ cube_center = torch.tensor([20., 20., 20., 1.])
 camera_positions = []
 for phi in np.linspace(0, math.pi, 4):
     for theta in np.linspace(0, 2 * math.pi, 5):
-        phi += math.pi/4
-        theta += math.pi/4
+        phi += math.pi / 4
+        theta += math.pi / 4
         x = radius * math.sin(phi) * math.cos(theta)
         y = radius * math.sin(phi) * math.sin(theta)
         z = radius * math.cos(phi)
@@ -24,7 +24,6 @@ for phi in np.linspace(0, math.pi, 4):
         z = 0 if abs(z) < 0.0001 else z
         camera_positions.append(torch.tensor([x, y, z, 0]))
 
-
 # print((torch.stack(camera_positions).unique(dim=0)) + cube_center)
 # print(torch.tensor(camera_positions))
 # print((torch.tensor(camera_positions) + cube_center).unique(dim=0))
@@ -32,7 +31,7 @@ for phi in np.linspace(0, math.pi, 4):
 radius = 50
 cube_center = torch.tensor([20., 20., 35., 1.])
 camera_positions = []
-for phi in np.linspace(0, math.pi, 72):
+for phi in np.linspace(2 * math.pi, 0, 72):
     x = radius * math.cos(phi)
     y = radius * math.sin(phi)
     z = 8
@@ -41,4 +40,5 @@ for phi in np.linspace(0, math.pi, 72):
     z = 0 if abs(z) < 0.0001 else z
     camera_positions.append(torch.tensor([x, y, z, 0]))
 
-print((torch.stack(camera_positions).unique(dim=0)) + cube_center)
+print((torch.stack(camera_positions)))
+print((torch.stack(camera_positions)) + cube_center)
